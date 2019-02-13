5f1e32bd28dcb5f89a28b2dde29a1fbfae6baa6a
Zongmin Lei <leizongmin@gmail.com>
2019-02-13 11:36:36 +0800

    fix: AdvancedCondition & AdvancedUpdate

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -31,48 +31,52 @@ export interface QueryOptionsParams {
   fields?: string[];
 }
 
-export type AdvancedCondition = Record<
-  string | number | symbol,
-  {
-    /** x IN y */
-    $in?: any[];
-    /** x NOT IN y */
-    $notIn?: any[];
-    /** x LIKE y */
-    $like?: string;
-    /** x NOT LIKE y */
-    $notLike?: string;
-    /** x = y */
-    $eq?: any;
-    /** x <> y */
-    $ne?: any;
-    /** x < y */
-    $lt?: any;
-    /** x <= y */
-    $lte?: any;
-    /** x > y */
-    $gt?: any;
-    /** x >= y */
-    $gte?: any;
-    /** x IS NULL */
-    $isNull?: true;
-    /** x IS NOT NULL */
-    $isNotNull?: true;
-    /** x = y (y不做任何转义) */
-    $raw?: string;
-  }
->;
+export type BaseFieldType = number | string | boolean | Date | null;
 
-export type AdvancedUpdate = Record<
-  string | number | symbol,
-  {
-    /** x = x + y */
-    $incr?: number;
-    /** x = x - y */
-    $decr?: number;
-    /** x = y (y不做任何转义) */
-    $raw: string;
-  }
->;
+export type AdvancedCondition = Record<string | number | symbol, AdvancedConditionField>;
+
+export type AdvancedConditionField =
+  | BaseFieldType
+  | {
+      /** x IN y */
+      $in?: any[];
+      /** x NOT IN y */
+      $notIn?: any[];
+      /** x LIKE y */
+      $like?: string;
+      /** x NOT LIKE y */
+      $notLike?: string;
+      /** x = y */
+      $eq?: any;
+      /** x <> y */
+      $ne?: any;
+      /** x < y */
+      $lt?: any;
+      /** x <= y */
+      $lte?: any;
+      /** x > y */
+      $gt?: any;
+      /** x >= y */
+      $gte?: any;
+      /** x IS NULL */
+      $isNull?: true;
+      /** x IS NOT NULL */
+      $isNotNull?: true;
+      /** x = y (y不做任何转义) */
+      $raw?: string;
+    };
+
+export type AdvancedUpdate = Record<string | number | symbol, AdvancedUpdateField>;
+
+export type AdvancedUpdateField =
+  | BaseFieldType
+  | {
+      /** x = x + y */
+      $incr?: number;
+      /** x = x - y */
+      $decr?: number;
+      /** x = y (y不做任何转义) */
+      $raw: string;
+    };
 
 export type DataRow = Record<string, any>;
